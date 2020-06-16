65d246af14423dcbf3ee6626475eb7fa95cc8f99
Yesui Zagdsuren <yesui@Yesuis-MacBook-Pro.local>
2020-06-16 13:30:17 +0800

    ingredient

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,2 +1,49 @@
 class IngredientController < ApplicationController
+  def index
+      @ingres = Ingredient.all
+  end
+
+  def show
+    @ingre = Ingredient.find(params[:id])
+  end
+
+  def new
+    @ingre = Ingredient.new
+  end
+
+  def edit
+      @ingre = Ingredient.find(params[:id])
+  end
+
+  def create
+    @ingre = Ingredient.new(ingre_params)
+
+    if @ingre.save
+      redirect_to @ingre
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @ingre = Ingredient.find(params[:id])
+
+    if @ingre.update(ingre_params)
+      redirect_to @ingre
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @ingre = Ingredient.find(params[:id])
+    @ingre.destroy
+
+    redirect_to ingredient_index_path
+  end
+
+  private
+    def ingre_params
+      params.require(:ingredient).permit(:name, :categoryId, :calories)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'ingredient/new'
+
+  resources :ingredient
+
+  root 'ingredient#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
