44d6ed909bd0dd008aca80550ae9dd8dd1e2642e
Uitumen <t.uitumen@gmai.com>
2020-06-16 17:30:37 +0800

    Category

--- db/migrate/20200616065539_create_categories.rb
+++ db/migrate/20200616065539_create_categories.rb
@@ -1,7 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-      t.string :category
+      t.string :categoryId
+      t.string :Name
 
       t.timestamps
     end
