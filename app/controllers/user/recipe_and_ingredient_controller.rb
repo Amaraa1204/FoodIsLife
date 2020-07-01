246fc5d64e08eb22580a660f3dfcd6766e6b8bea
Amaraa1204 <e.amarzul@gmail.com>
2020-07-01 10:54:32 +0800

    recipe with ingredients

--- app/controllers/user/recipe_and_ingredient_controller.rb
+++ app/controllers/user/recipe_and_ingredient_controller.rb
@@ -1,14 +1,16 @@
 class User::RecipeAndIngredientController < UserApplicationController
   def new
+    @recipe_and_ingredients = RecipeAndIngredient.new
   end
 
   def create 
-    
+    @rip = RecipeAndIngredient.new(@recipe_and_ingredients_params)
+		@rip.save
   end
 
   private
 
   def recipe_and_ingredients_params
-    params.require(:recipe_and_ingredients).permit(:recipe_id, {:ingridient_id => []})
+    params.require(:recipe_and_ingredients).permit(:recipe_id, :ingredient_id)
   end
 end
